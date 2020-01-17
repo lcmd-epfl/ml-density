247d392dd6407238c0fb29c59f76b89611c3d4cc
Ksenia <ksenia.briling@epfl.ch>
2020-01-17 18:05:11 +0100

    Parallelize kernels.py using mpi4py

--- SOURCE/kernels.py
+++ SOURCE/kernels.py
@@ -6,6 +6,8 @@ from basis import basis_read
 from functions import *
 from power_spectra import read_ps_1mol
 
+USEMPI = 1
+
 conf = Config()
 
 def set_variable_values():
@@ -51,13 +53,16 @@ power_ref_sparse = {}
 for l in range(llmax+1):
     power_ref_sparse[l] = np.load(powerrefbase+str(l)+"_"+str(M)+".npy");
 
-# compute sparse kernel matrix
-for iconf in range(ndata):
 
-    print_progress(iconf, ndata)
+#------------------------------------------------------------------------
+
+def kernel_for_mol(iconf):
+    if USEMPI==0:
+      print_progress(iconf, ndata)
+    else:
+      print(nproc, ':', iconf-start[nproc], flush=1)
 
     atoms = atomic_numbers[iconf]
-    # define sparse indexes
     kernel_size = 0
     kernel_sparse_indexes = np.zeros((M,natoms[iconf],llmax+1,2*llmax+1,2*llmax+1),int)
     for iref in range(M):
@@ -97,4 +102,35 @@ for iconf in range(ndata):
                             ik = kernel_sparse_indexes[iref,iatspe,l,im1,im2]
                             k_NM[ik] = kern[im2,im1]
     np.savetxt(kernelconfbase+str(iconf)+".dat", k_NM,fmt='%.06e')
+
+#------------------------------------------------------------------------
+if USEMPI==0:
+    for iconf in range(ndata):
+        kernel_for_mol(iconf)
+
+else:
+  from mpi4py import MPI
+  comm = MPI.COMM_WORLD
+  Nproc = comm.Get_size()
+  nproc = comm.Get_rank()
+
+  msg = "proc " + "%3d"%(nproc) + ' : ' + MPI.Get_processor_name()
+  if nproc == 0:
+    print(msg)
+    for i in range(1, Nproc):
+      msg = comm.recv(source=i)
+      print (msg)
+    print(flush=1)
+  else:
+    comm.send(msg, dest=0)
+  comm.barrier()
+
+  start = [0]*(Nproc+1)
+  div = ndata//Nproc
+  rem = ndata%Nproc
+  for i in range(0,Nproc):
+    start[i+1] = start[i] + ( (div+1) if (i<rem) else div )
+  for iconf in range(start[nproc], start[nproc+1]):
+      kernel_for_mol(iconf)
+  print(nproc, ':', 'finished')
 
