7d39e200d8a10adeee7d7729359022c03962274a
Ksenia Briling <30023616+briling@users.noreply.github.com>
2021-05-20 09:32:39 +0200

    Add files via upload

--- 0_prepare.bash
+++ 0_prepare.bash
@@ -1,26 +1,15 @@
-mols="quantum-chemistry/geometries_dimers/*.xyz quantum-chemistry/geometries_monomers/*.xyz"
+mols=$(ls quantum-chemistry/geometries/*.xyz | sort -V)
 
 cat ${mols} > ALL.xyz
 
-mkdir ALL_J ALL_Cj ALL_S ALL_Cs ALL_Csn ALL_Cjn
+mkdir ALL_C ALL_S
 k=0
 for mol in $mols; do
-  name=${mol##*/}
-  name=${name%%.xyz*}
-  printf "%5d   %s\n" $k $name
-
-  j=quantum-chemistry/calculations_pyscf/${name}/Jmat.npy
-  cj=quantum-chemistry/calculations_pyscf/${name}/coeffs.dat
-  cjn=quantum-chemistry/s_j_fitting_correct_N/${name}.cjn.dat
-  s=quantum-chemistry/calculations_q/${name}.s.npy
-  cs=quantum-chemistry/calculations_q/${name}.cs.dat
-  csn=quantum-chemistry/s_j_fitting_correct_N/${name}.csn.dat
-  ln -s $(readlink -m ${j}) "ALL_J/mol_${k}.npy"
+  printf "%5d   %s\n" $k $mol
+  c=${mol}.coef.dat
+  s=${mol}.smat.npy
+  ln -s $(readlink -m ${c}) "ALL_C/mol_${k}.dat"
   ln -s $(readlink -m ${s}) "ALL_S/mol_${k}.npy"
-  ln -s $(readlink -m ${cj}) "ALL_Cj/mol_${k}.dat"
-  ln -s $(readlink -m ${cs}) "ALL_Cs/mol_${k}.dat"
-  ln -s $(readlink -m ${cjn}) "ALL_Cjn/mol_${k}.dat"
-  ln -s $(readlink -m ${csn}) "ALL_Csn/mol_${k}.dat"
   k=$(($k+1))
 done
 
