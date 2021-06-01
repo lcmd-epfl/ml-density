2a8409344e23ac2433da9abf72ffad53a81b0eb2
Ksenia <ksenia.briling@epfl.ch>
2021-06-01 18:24:14 +0200

    Correct number of eletrons for extrapolation (unit matrix)

--- SOURCE/extrapolation.py
+++ SOURCE/extrapolation.py
@@ -3,7 +3,7 @@
 import sys
 import numpy as np
 from config import Config,get_config_path
-from functions import moldata_read,get_elements_list,get_atomicindx,prediction2coefficients,gpr2pyscf,number_of_electrons,averages_read
+from functions import moldata_read,get_elements_list,get_atomicindx,prediction2coefficients,gpr2pyscf,number_of_electrons,averages_read,number_of_electrons_ao
 from basis import basis_read_full
 from run_prediction import run_prediction
 
@@ -29,6 +29,14 @@ xyzexfilename   = conf.paths['ex_xyzfile']
 kernelexbase    = conf.paths['ex_kernel_base']
 predictfilebase = conf.paths['ex_predict_base']
 outfilebase     = conf.paths['ex_output_base']
+
+if 'ex_charges' in conf.paths:
+  correct_n      = True
+  exchargesfiles = conf.paths['ex_charges']
+  charges        = np.loadtxt(exchargesfiles, dtype=int)
+  print(charges)
+else:
+  correct_n      = False
 
 (nmol, natoms, atomic_numbers) = moldata_read(xyzfilename)
 (nmol_ex, natoms_ex, atomic_numbers_ex) = moldata_read(xyzexfilename)
@@ -57,12 +65,23 @@ pred = run_prediction(nmol_ex, natmax_ex, natoms_ex,
 av_coefs = averages_read(elements_ex, avdir)
 for imol in range(nmol_ex):
     atoms = atomic_numbers_ex[imol]
+    nel_exact = sum(atoms)
+    if correct_n!=None:
+      nel_exact -= charges[imol]
+
     rho  = prediction2coefficients(atoms, lmax, nmax, pred[imol], av_coefs)
     rho1 = gpr2pyscf              (atoms, lmax, nmax, rho)
     nel = number_of_electrons(basis, atoms, rho1)
     strg = "mol # %*i :  %8.4f / %3d (%.1e)"%(
-        len(str(nmol_ex)), imol, nel, sum(atoms), nel-sum(atoms) )
+        len(str(nmol_ex)), imol, nel, nel_exact, nel-nel_exact )
     print(strg)
     np.savetxt(outfilebase+'gpr_'  +str(imol)+'.dat', rho)
     np.savetxt(outfilebase+'pyscf_'+str(imol)+'.dat', rho1)
+
+    if correct_n!=None:
+      q  = number_of_electrons_ao(basis, atoms)
+      rho_u = rho + q * (nel_exact-q@rho) / (q@q)
+      rho_u1 = gpr2pyscf(atoms, lmax, nmax, rho_u)
+      np.savetxt(outfilebase+'gpr_'  +str(imol)+'.unit.dat', rho_u)
+      np.savetxt(outfilebase+'pyscf_'+str(imol)+'.unit.dat', rho_u1)
 
