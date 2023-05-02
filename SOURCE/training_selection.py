b8cea72eff1c90923b5105d3fa95f1c41c201b4b
Ksenia <ksenia.briling@epfl.ch>
2023-05-02 13:55:56 +0200

    Rewrite training_selection.py

--- SOURCE/training_selection.py
+++ SOURCE/training_selection.py
@@ -2,26 +2,28 @@
 
 import sys
 import numpy as np
-from config import Config,get_config_path
+from config import Config, get_config_path
 import ase.io
 
-path = get_config_path(sys.argv)
-conf = Config(config_path=path)
 
-def set_variable_values():
+def set_variable_values(conf):
     seed  = conf.get_option('seed'        ,    1, int  )
     train = conf.get_option('train_size'  , 1000, int  )
     return [seed, train]
 
-[seed, train] = set_variable_values()
+def main():
+    path = get_config_path(sys.argv)
+    conf = Config(config_path=path)
+    [seed, train] = set_variable_values(conf)
+    xyzfilename      = conf.paths['xyzfile']
+    trainfilename    = conf.paths['trainingselfile']
 
-xyzfilename      = conf.paths['xyzfile']
-trainfilename    = conf.paths['trainingselfile']
+    xyzfile = ase.io.read(xyzfilename,":")
+    nmol = len(xyzfile)
+    np.random.seed(seed)
+    output = np.random.choice(nmol, train, replace=False)
+    np.savetxt(trainfilename, output, fmt='%i')
 
-xyzfile = ase.io.read(xyzfilename,":")
-nmol = len(xyzfile)
 
-np.random.seed(seed)
-output = np.random.choice(nmol, train, replace=False)
-np.savetxt(trainfilename, output, fmt='%i')
-
+if __name__=='__main__':
+    main()
