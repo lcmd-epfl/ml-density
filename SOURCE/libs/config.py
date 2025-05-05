acf6c3e07fb160a6fa2b24f5318cf97b611eb0d7
Ksenia <ksenia.briling@epfl.ch>
2025-05-05 10:51:18 +0200

    Automatically check config and create dirs

--- SOURCE/libs/config.py
+++ SOURCE/libs/config.py
@@ -104,6 +104,66 @@ def read_config(argv):
 
     path = get_config_path(argv)
     conf = Config(config_path=path)
+    check_paths(conf)
     o = set_variable_values()
     p = get_all_paths()
     return o, p
+
+
+def check_paths(conf):
+    paths0 = [
+      'xyzfile',
+      'basisfile',
+      'ex_xyzfile',
+      'chargesfile',
+      'averages_file'
+      ]
+
+    paths1 = [
+      'avec_base',
+      'baselined_w_base',
+      'bmat_base',
+      'coeff_base',
+      'ex_kernel_base',
+      'ex_output_base',
+      'goodcoef_base',
+      'goodover_base',
+      'kernel_conf_base',
+      'kmm_base',
+      'output_base',
+      'over_base',
+      'predict_base',
+      'ps_ref_base',
+      'ps_split_base',
+      'refs_sel_base',
+      'qrefs_sel_base',
+      'weights_base',
+      'trainingselfile',
+      'ex_ps_base',
+      ]
+
+    for key in paths0:
+        if key in conf.paths:
+            path = conf.paths[key]
+            isfile = os.path.isfile(path)
+            if not isfile:
+                print(f'Cannot find file "{path}" ("{key}")')
+        else:
+            print(f'Cannot find option "{key}"')
+
+    dirs = []
+    for key in paths1:
+      if key in conf.paths:
+          path = conf.paths[key]
+          path = os.path.dirname(path)
+          isdir = os.path.isdir(path)
+          if not isdir:
+            #print(f'Cannot find directory "{path}" ("{key}")')
+            dirs.append(path)
+      else:
+        print(f'Cannot find option "{key}"')
+    print()
+
+    for d in sorted(set(dirs)):
+        print(f'Creating directory {d}')
+        os.makedirs(d)
