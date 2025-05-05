aca5d3e9177a826fd177a756feeb2ba08643856f
Ksenia <ksenia.briling@epfl.ch>
2025-05-05 12:21:51 +0200

    Add example data

---
+++ sidechains_examples/ALL.bash
@@ -0,0 +1,14 @@
+path=../GIT/SOURCE
+#$path/project.py
+#$path/training_selection.py
+#$path/get_power_spectra.py
+
+$path/environments.py
+$path/power_spectra_ref.py
+$path/rmatrix.py
+$path/kernels.py
+$path/get_matrices.py
+$path/get_matrices.py b
+$path/regression.py
+$path/prediction.py
+$path/compute_error.py > error.txt
