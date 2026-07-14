lcmd-epfl/ml-density

commit 39d657bf679c92c28498c465ca5383bd97843638

BREAKING CHANGES: Remove extrapolation power spectra and kernel scripts
* `src/ex_kernels.py`       -> `src/kernels.py --extra`
* `src/ex_power_spectra.py` -> `src/get_power_spectra.py --extra`

TODO: same for prediction / compute_errors / extrapolation

src/get_power_spectra.py
@@ -2,47 +2,82 @@
 """Compute λ-SOAP power spectra for the dataset molecules."""
 
 import os
+import numpy as np
 import ase.io
-import metatensor
-from tqdm import trange
 from libs.config import get_settings
-from libs.lsoap import generate_lambda_soap_wrapper, make_rascal_hypers
-from libs.functions import get_elements, Basis
-from libs.multi import multi_process
+from libs.functions import get_elements, moldata_read, Basis, get_dataset_paths
 from libs.logger_setup import setup_logger
+from libs.config_utils import defaults
+from libs.lsoap import generate_lambda_soap_wrapper, make_rascal_hypers
+from libs.multi import process_molecules
+import metatensor
 
 logger = setup_logger(__name__, __file__)
 
 
 def main():  # noqa: D103
-    args, o, p = get_settings(return_args=['missing_only', 'mpi'])
+    args, o, p = get_settings(return_args=['missing_only', 'mpi', 'extra'])
+    dataset_paths = get_dataset_paths(p, args.extra)
+    run_power_spectrum(dataset_paths, p.xyzfilename, o, missing_only=args.missing_only, use_mpi=args.mpi)
 
+
+def run_power_spectrum(dataset_paths, xyz_path_main, o, *, missing_only=False, use_mpi=defaults.mpi):
+    """Compute and save per-molecule power spectra.
+
+    Args:
+        dataset_paths (.libs.functions.DatasetPaths): Paths and path templates
+                      (XYZ, power spectra, kernel) for the main or out-of-sample dataset.
+        xyz_path_main (str): Path to the *main* dataset XYZ file.
+        o (SimpleNamespace): Options parsed from config.
+        missing_only (bool): Skip molecules with existing output files.
+        use_mpi (bool): Whether to use MPI for process distribution.
+    """
     def do_mol(imol):
-        ppath = p.power_spectrum.format(imol)
-        if args.missing_only and os.path.exists(ppath):
+        ppath = dataset_paths.power.format(imol)
+        if missing_only and os.path.exists(ppath):
             return
         soap = generate_lambda_soap_wrapper(mols[imol], rascal_hypers, neighbor_species=elements,
-                                            normalize=o.ps_normalize, min_norm=o.ps_min_norm,
-                                            lmax=basis.lmax)
+                                            normalize=o.ps_normalize, min_norm=o.ps_min_norm, lmax=basis.lmax)
         metatensor.save(ppath, soap)
 
-    rascal_hypers = make_rascal_hypers(o.soap_rcut, o.soap_ncut, o.soap_lcut, o.soap_sigma)
+    mols, elements = read_mol_elements(dataset_paths.xyz, xyz_path_main)
 
-    mols = ase.io.read(p.xyzfilename, ":")
-    elements = get_elements(mols)
     basis = Basis(o.basisname, elements)
-
+    rascal_hypers = make_rascal_hypers(o.soap_rcut, o.soap_ncut, o.soap_lcut, o.soap_sigma)
     logger.debug(f'rascal_hypers={rascal_hypers}')
     logger.debug(f'elements={elements}')
     logger.debug(f'basis.lmax={basis.lmax}')
     logger.debug(f'ps_min_norm={o.ps_min_norm} ps_normalize={o.ps_normalize}')
+    process_molecules(len(mols), do_mol, use_mpi=use_mpi)
 
-    nmol = len(mols)
-    if args.mpi:
-        multi_process(nmol, do_mol)
+
+def read_mol_elements(xyz_this_set, xyz_main_set):
+    """Check if out-of-sample elements are present in the dataset.
+
+    Args:
+        xyz_this_set (str): Path to the XYZ file (main or out-of-sample dataset).
+        xyz_main_set (str): Path to the main dataset XYZ file.
+
+    Returns:
+        np.ndarray[int]: Elements present in the *main* dataset.
+
+    Raises:
+        RuntimeError: Out-of-sample set contains elements that are not in the main dataset.
+    """
+    mols = ase.io.read(xyz_this_set, ":")
+    q_this_set = get_elements(mols)
+
+    if xyz_this_set==xyz_main_set:
+        q = q_this_set
     else:
-        for imol in trange(nmol):
-            do_mol(imol)
+        q_main_set = get_elements(moldata_read(xyz_main_set))
+        if not set(q_this_set).issubset(q_main_set):
+            with np.printoptions(legacy="1.25"):
+                msg = f'Out-of-sample molecules contain elements not present in the dataset: {set(q_this_set).difference(q_main_set)}'
+            raise RuntimeError(msg)
+        logger.debug(f'OSS contains: {q_this_set}')
+        q = q_main_set
+    return mols, q
 
 
 if __name__=='__main__':

src/kernels.py
@@ -3,34 +3,41 @@
 
 import os
 import metatensor
-from tqdm import trange
 from libs.config import get_settings
-from libs.functions import moldata_read
-from libs.kernels_lib import kernel_for_mol
-from libs.multi import multi_process
 from libs.logger_setup import setup_logger
+from libs.functions import moldata_read, get_dataset_paths
+from libs.kernels_lib import kernel_for_mol
+from libs.multi import process_molecules
+from libs.config_utils import defaults
 
 logger = setup_logger(__name__, __file__)
 
 
 def main():  # noqa: D103
-    args, _, p = get_settings(return_args=['missing_only', 'mpi'])
+    args, _, p = get_settings(return_args=['missing_only', 'mpi', 'extra'])
+    dataset_paths = get_dataset_paths(p, args.extra)
+    run_kernel(dataset_paths, p.reference_power_spectra, missing_only=args.missing_only, use_mpi=args.mpi)
 
+
+def run_kernel(dataset_paths, ref_power_path, *, missing_only=False, use_mpi=defaults.mpi):
+    """Compute and save per-molecule kernels.
+
+    Args:
+        dataset_paths (.libs.functions.DatasetPaths): Paths and path templates
+                      (XYZ, power spectra, kernel) for the main or out-of-sample dataset.
+        ref_power_path (str): Path to the reference power spectra.
+        missing_only (bool): Skip molecules with existing kernel files.
+        use_mpi (bool): Whether to use MPI for process distribution.
+    """
     def do_mol(imol):
-        kpath = p.kernel_nm.format(imol)
-        if args.missing_only and os.path.exists(kpath):
+        kpath = dataset_paths.kernel.format(imol)
+        if missing_only and os.path.exists(kpath):
             return
-        kernel_for_mol(atomic_numbers[imol], power_ref, p.power_spectrum.format(imol), kpath)
+        kernel_for_mol(atomic_numbers[imol], power_ref, dataset_paths.power.format(imol), kpath)
 
-    atomic_numbers = moldata_read(p.xyzfilename)
-    power_ref = metatensor.load(p.reference_power_spectra)
-    nmol = len(atomic_numbers)
-
-    if args.mpi:
-        multi_process(nmol, do_mol)
-    else:
-        for imol in trange(nmol):
-            do_mol(imol)
+    atomic_numbers = moldata_read(dataset_paths.xyz)
+    power_ref = metatensor.load(ref_power_path)
+    process_molecules(len(atomic_numbers), do_mol, use_mpi=use_mpi)
 
 
 if __name__=='__main__':

ruff.toml
@@ -59,3 +59,4 @@ ignore = [
 "src/get_matrices_C.py" = ["COM819"]  # prohibited-trailing-comma
 "src/libs/tmap.py"      = ["E251", "PLC2701"]  # unexpected-spaces-around-keyword-parameter-equals, import-private-name
 "src/libs/lsoap.py"     = ["PLC0415", "PLC2701"]  # import-outside-top-level, import-private-name
+"src/libs/multi.py"     = ["PLC0415"]  # import-outside-top-level

src/ex_kernels.py
@@ -1,25 +1,0 @@
-#!/usr/bin/env python3
-"""Compute kernel for out-of-sample molecules."""
-
-import metatensor
-from tqdm import tqdm
-from libs.config import get_settings
-from libs.functions import moldata_read
-from libs.kernels_lib import kernel_for_mol
-from libs.logger_setup import setup_logger
-
-logger = setup_logger(__name__, __file__)
-
-
-def main():  # noqa: D103
-    _, p = get_settings()
-
-    atomic_numbers_ex = moldata_read(p.xyzexfilename)
-    power_ref = metatensor.load(p.reference_power_spectra)
-
-    for imol, atoms in enumerate(tqdm(atomic_numbers_ex)):
-        kernel_for_mol(atoms, power_ref, p.extra_power_spectrum.format(imol), p.extra_kernel_nm.format(imol))
-
-
-if __name__=='__main__':
-    main()

src/ex_power_spectra.py
@@ -1,43 +1,0 @@
-#!/usr/bin/env python3
-"""Compute power spectra for out-of-sample molecules."""
-
-import ase.io
-import metatensor
-from tqdm import tqdm
-from libs.config import get_settings
-from libs.lsoap import generate_lambda_soap_wrapper, make_rascal_hypers
-from libs.functions import get_elements, moldata_read, Basis
-from libs.logger_setup import setup_logger
-
-logger = setup_logger(__name__, __file__)
-
-
-def main():  # noqa: D103
-    o, p = get_settings()
-
-    mols_ex = ase.io.read(p.xyzexfilename, ":")
-    atomic_numbers = moldata_read(p.xyzfilename)
-
-    elements = get_elements(atomic_numbers)
-    elements_ex = get_elements(mols_ex)
-    if not set(elements_ex).issubset(elements):
-        msg = f'Different elements in the molecule and in the training set: {elements_ex} and {elements}'
-        raise RuntimeError(msg)
-
-    basis = Basis(o.basisname, elements=elements_ex)
-    rascal_hypers = make_rascal_hypers(o.soap_rcut, o.soap_ncut, o.soap_lcut, o.soap_sigma)
-
-    logger.debug(f'rascal_hypers={rascal_hypers}')
-    logger.debug(f'elements={elements}')
-    logger.debug(f'basis.lmax={basis.lmax}')
-    logger.debug(f'ps_min_norm={o.ps_min_norm} ps_normalize={o.ps_normalize}')
-
-    for imol, mol in enumerate(tqdm(mols_ex)):
-        soap = generate_lambda_soap_wrapper(mol, rascal_hypers, neighbor_species=elements,
-                                            normalize=o.ps_normalize, min_norm=o.ps_min_norm,
-                                            lmax=basis.lmax)
-        metatensor.save(p.extra_power_spectrum.format(imol), soap)
-
-
-if __name__=='__main__':
-    main()

src/extrapolation.py
@@ -17,7 +17,7 @@ logger = setup_logger(__name__, __file__)
 def main():  # noqa: D103
     o, p = get_settings()
 
-    atomic_numbers = moldata_read(p.xyzexfilename)
+    atomic_numbers = moldata_read(p.extra_xyzfilename)
     averages = metatensor.load(p.spherical_averages)
     ref_elements = pd.read_csv(p.reference_environments)['q'].to_numpy()
     basis = Basis(o.basisname, elements=ref_elements)

src/libs/config_cli.py
@@ -35,6 +35,7 @@ def parse_cli_args(return_args=None):
     add_argument(parser, "--training", dest='training', action='store_true', help='run prediction / compute error on the training set instead of the test set')
     add_argument(parser, "-b", "--b", dest="get_b_matrix", action='store_true', help='if True, get_matrices computes the "B matrix"; if False, the "A vector"')
     add_argument(parser, "--missing-only", dest="missing_only", action='store_true', help='dangerous: not recompute existing power spectra / kernels')
+    add_argument(parser, "--extra", "--extrapolation", "--oos", dest="extra", action='store_true', help='run script for an out-of-sample set (extrapolation)')
 
     mpi = parser.add_mutually_exclusive_group()
     mpi.add_argument("--mpi-dummy-argument", dest="mpi", default=defaults.mpi, action='store_true', help=argparse.SUPPRESS)

src/libs/config_parser.py
@@ -3,7 +3,7 @@
 import os
 import configparser
 import logging
-from libs.config_utils import WhenMissing, CheckFile, defaults, OptSpecs, PathSpecs, Choice
+from libs.config_utils import WhenMissing, CheckFile, OptSpecs, PathSpecs, Choice
 
 logger = logging.getLogger('__main__')
 
@@ -23,7 +23,6 @@ class Config:
                 self.add_group(group)
                 for dest, spec in entries.items():
                     self.add_entry(group, dest, spec)
-
 
     def add_group(self, group):
         """Register a new parsing group.

commit 7cd94a8da1c3cdb4749e7fd0b6fa5b1217744337

Fix pipeline-breaking regressions in kernels.py, get_power_spectra.py, compute_error.py
get_dataset_paths() takes extra as keyword-only; kernels.py and
get_power_spectra.py were still calling it positionally (regressed by
39d657b), raising TypeError on every run -- the first two steps of
ALL.bash were broken.

compute_error.py's electron-count diagnostic line was missing its f
prefix (regressed by 3842175), silently printing the literal
'ΔN: {total.N:.2e}' instead of the value whenever use_charges is set.

src/compute_error.py
@@ -137,7 +137,7 @@ def main():  # noqa: D103
         total /= npred
         print(''.join([
             f'\nfrac={frac}\tMAE = {total.rel_bl:.2e} %  {total.rel:.2e} %    ( {total.abs:.2e} )',
-            '  ΔN: {total.N:.2e}' if o.use_charges else '',
+            f'  ΔN: {total.N:.2e}' if o.use_charges else '',
             ]))
 
 

src/get_power_spectra.py
@@ -17,7 +17,7 @@ logger = setup_logger(__name__, __file__)
 
 def main():  # noqa: D103
     args, o, p = get_settings(return_args=['missing_only', 'mpi', 'extra'])
-    dataset_paths = get_dataset_paths(p, args.extra)
+    dataset_paths = get_dataset_paths(p, extra=args.extra)
     run_power_spectrum(dataset_paths, p.xyzfilename, o, missing_only=args.missing_only, use_mpi=args.mpi)
 
 

src/kernels.py
@@ -15,7 +15,7 @@ logger = setup_logger(__name__, __file__)
 
 def main():  # noqa: D103
     args, _, p = get_settings(return_args=['missing_only', 'mpi', 'extra'])
-    dataset_paths = get_dataset_paths(p, args.extra)
+    dataset_paths = get_dataset_paths(p, extra=args.extra)
     run_kernel(dataset_paths, p.reference_power_spectra, missing_only=args.missing_only, use_mpi=args.mpi)
 
 